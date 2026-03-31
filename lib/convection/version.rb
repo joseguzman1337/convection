53a4c27cb6eb7ed38a73937355ed647b045d8b7f
jose guzman <joseguzman1337@users.noreply.github.com>
2026-03-31 00:50:58 -0500

    [CRUSH] chore(repo): checkpoint pending changes #TASK-000

--- lib/convection/version.rb
+++
@@ -1,4 +0,0 @@
-# nodoc
-module Convection
-  VERSION = IO.read(File.expand_path('../../../VERSION', __FILE__)) rescue '0.0.1'
-end
--- yard_extensions.rb
+++
@@ -1,4 +0,0 @@
-# Load all Ruby files inside of the YARD extensions directory.
-Dir.glob('yard_extensions/**.rb') do |file|
-  require_relative file
-end
